c7f2f4ce6ded44d2e1a1d19fecd41ae1b850b769
Jan Kaduch <kaduch05@gmail.com>
2025-11-01 14:44:01 +0100

    add heap allocation for match_file and validating first line of match file

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#pragma once
 #include <stdio.h>
 #include "header/stats.h"
 #include "header/utility.h"
@@ -7,7 +6,7 @@
 int main(int argc, char **argv) {
 
     // pridate treti argument jako test
-    if (!validate_argument(argv[1]) && !validate_argument(argv[1])) {
+    if (!validate_argument(argv[1]) && !validate_argument(argv[2])) {
         // stars the main program
         start_stats(argv[1], argv[2], "test");
         return 0;
--- src/stats.c
+++ src/stats.c
@@ -1,9 +1,61 @@
 #pragma once
 #include <stdio.h>
 #include "../header/stats.h"
+#include <stdlib.h>
+#include <string.h>
+
 #include "../header/utility.h"
 
-void start_stats(char* file_match, char* file_player_names, char* file_output) {
+void remove_tailing(char* line)
+{
+    if (!line) return;
+    line[strcspn(line, "\r\n")] = '\0'; // odstrani \n a \r z konce retezce
+}
 
-    printf("zacinam");
+int handle_player_names_file(char player_id, FILE* file_player_names)
+{
+    char* buffer = malloc(256 * sizeof(char)); // nastavim max 256 znaku na jeden radek
+    if (!buffer)
+    {
+        printf("Chyba alokace pameti\n");
+        return 1;
+    }
+    while (fgets(buffer, sizeof(buffer), file_player_names))
+    {
+        printf("Player name: %s", buffer);
+    }
+    free(buffer);
+}
+
+int handle_match_file(FILE* file_match, FILE* file_player_names)
+{
+    char* buffer = malloc(256 * sizeof(char)); // nastavim max 256 znaku na jeden radek
+    if (!buffer)
+    {
+        printf("Chyba alokace pameti\n");
+        return 1;
+    }
+    fgets(buffer, sizeof(buffer), file_match);
+    remove_tailing(buffer);
+    if (strcmp(buffer, "match") != 0)
+    {
+        printf("Chyba formatu souboru: ocekavano 'match', nalezeno '%s'\n", buffer);
+        free(buffer);
+        return 1;
+    }
+    handle_player_names_file('1', file_player_names);
+
+
+    close_file(file_match);
+    close_file(file_player_names);
+    free(buffer);
+    return 0;
+}
+
+void start_stats(char* file_match, char* file_player_names, char* file_output)
+{
+    printf("zacinam\n");
+    handle_match_file(read_file(file_match), read_file(file_player_names));
+
+
 }
