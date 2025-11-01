0aa2cbedbf497c4b883a186d714e7992ea5e51a5
Jan Kaduch <kaduch05@gmail.com>
2025-11-01 16:23:36 +0100

    implement strict argument validation

--- main.c
+++ main.c
@@ -4,11 +4,16 @@
 
 
 int main(int argc, char **argv) {
-
+    // check number of arguments
+    if (argc != 4)
+    {
+        printf("spatny pocet argumentu");
+        return 1;
+    }
     // pridate treti argument jako test
-    if (!validate_argument(argv[1]) && !validate_argument(argv[2])) {
+    if (!validate_argument(argv[1]) && !validate_argument(argv[2]) && !validate_argument(argv[3])) {
         // stars the main program
-        start_stats(argv[1], argv[2], "test");
+        start_stats(argv[1], argv[2], argv[3]);
         return 0;
 
     }
