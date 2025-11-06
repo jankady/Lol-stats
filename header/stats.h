852fd16f7ce4f507891795e38470f66719d87ab4
jankady <kaduch05@gmai.com>
2025-11-06 18:02:27 +0100

    add getters and setters. make handle_player_names_file cleaner

--- header/stats.h
+++ header/stats.h
@@ -1,9 +1,10 @@
 #pragma once
+#include "league_player.h"
 
 void remove_tailing(char* line);
 
 int handle_output_file(char* file_output_path);
 
-int handle_player_names_file(char player_id, FILE* file_player_names);
+int handle_player_names_file(int player_id, const league_players_array_t* players_array);
 
 int start_stats(const char* arg1, const char* arg2, char* arg3);
--- src/stats.c
+++ src/stats.c
@@ -9,52 +9,36 @@
 #define MAX_PLAYERS_NUM 20
 #define MAX_BUFFER_SIZE 256
 
-void remove_tailing(char* line)
-{
+void remove_tailing(char *line) {
     if (!line) return;
     line[strcspn(line, "\r\n")] = '\0'; // odstrani \n a \r z konce retezce
 }
 
-int handle_output_file(char* file_output_path)
-{
+int handle_output_file(char *file_output_path) {
     //add later
     return 0;
 }
 
-int handle_player_names_file(char player_id, FILE* file_player_names)
-{
-    char* buffer = malloc(MAX_BUFFER_SIZE * sizeof(char)); // nastavim max 256 znaku na jeden radek
-    if (!buffer)
-    {
-        fprintf(stderr, "Chyba alokace pameti pro player_name\n");
-        return 1;
-    }
-    rewind(file_player_names); // reset file pointer to the beginning of the file
-    while (fgets(buffer, MAX_BUFFER_SIZE, file_player_names))
-    {
-        if (player_id == buffer[0])
-        {
-            free(buffer);
+int handle_player_names_file(int player_id, const league_players_array_t *players_array) {
+
+    for (int i = 0; i < players_array->count; i++) {
+        if (players_array->players[i].player_id == player_id) {
             return 0;
         }
     }
-    free(buffer);
+
     return 1;
 }
 
-int player_exist(char* buffer, FILE* file_player_names)
-{
+int player_exist(char *buffer, league_players_array_t *players_array) {
     remove_tailing(buffer);
-    for (int i = 0; i < 3; i++)
-    {
-        if ((unsigned long)i*2 >= strlen(buffer) || strlen(buffer) != 5)
-        {
+    for (int i = 0; i < 3; i++) {
+        if ((unsigned long) i * 2 >= strlen(buffer) || strlen(buffer) != 5) {
             fprintf(stderr, "Chyba formatu souboru: ocekavano 'id,id,id', nalezeno '%s'\n", buffer);
             return 1;
         }
-        char player_id = buffer[i * 2];
-        if (handle_player_names_file(player_id, file_player_names) != 0)
-        {
+        int player_id = buffer[i * 2] - '0'; // convert char to int i subsctract asci values
+        if (handle_player_names_file(player_id, players_array) != 0) {
             fprintf(stderr, "Hrac s ID '%c' nebyl nalezen v souboru hracu\n", player_id);
 
             return 1;
@@ -63,37 +47,30 @@ int player_exist(char* buffer, FILE* file_player_names)
     return 0;
 }
 
-int check_word(char* buffer, const char* expected_word)
-{
+int check_word(char *buffer, const char *expected_word) {
     remove_tailing(buffer);
     if (strcmp(expected_word, "match") == 0) {
-        if (strcmp(buffer, "match") != 0)
-        {
+        if (strcmp(buffer, "match") != 0) {
             fprintf(stderr, "Chyba formatu souboru: ocekavano 'match', nalezeno '%s'\n", buffer);
             return 1;
         }
-    }
-    else if (strcmp(expected_word, "winner_team") == 0) {
-        if (strcmp(buffer, "red") != 0 && strcmp(buffer, "blue") != 0)
-        {
-            fprintf(stderr,"Chyba formatu souboru: ocekavano 'blue' nebo 'red', nalezeno '%s'\n", buffer);
+    } else if (strcmp(expected_word, "winner_team") == 0) {
+        if (strcmp(buffer, "red") != 0 && strcmp(buffer, "blue") != 0) {
+            fprintf(stderr, "Chyba formatu souboru: ocekavano 'blue' nebo 'red', nalezeno '%s'\n", buffer);
             return 1;
         }
-    }
-    else {
-        fprintf(stderr,"Chyba formatu souboru nalezeno neplatne slovo: '%s'\n", buffer);
+    } else {
+        fprintf(stderr, "Chyba formatu souboru nalezeno neplatne slovo: '%s'\n", buffer);
         return 1;
     }
 
     return 0;
 }
 
-int start_stats(const char* file_match_path, const char* file_player_names_path, char* file_output_path)
-{
-    FILE* file_match = read_file(file_match_path);
-    FILE* file_player_names = read_file(file_player_names_path);
-    if (file_match == NULL || file_player_names == NULL)
-    {
+int start_stats(const char *file_match_path, const char *file_player_names_path, char *file_output_path) {
+    FILE *file_match = read_file(file_match_path);
+    FILE *file_player_names = read_file(file_player_names_path);
+    if (file_match == NULL || file_player_names == NULL) {
         if (file_match)
             close_file(file_match);
         if (file_player_names)
@@ -103,30 +80,22 @@ int start_stats(const char* file_match_path, const char* file_player_names_path,
     clear_file(file_output_path);
     printf("zacinam\n");
     int buffer_size = MAX_BUFFER_SIZE;
-    char* buffer = malloc(MAX_BUFFER_SIZE * sizeof(char));
-    if (!buffer)
-    {
+    char *buffer = malloc(MAX_BUFFER_SIZE * sizeof(char));
+    if (!buffer) {
         fprintf(stderr, "Chyba alokace pameti pro stats_start\n");
         return 1;
     }
-    league_players_array_t* league_players = init_league_players(file_player_names, MAX_PLAYERS_NUM);
-    if (league_players == NULL)
-    {
+    league_players_array_t *league_players = init_league_players(file_player_names, MAX_PLAYERS_NUM);
+    if (league_players == NULL) {
         free(buffer);
         close_file(file_match);
         close_file(file_player_names);
         return 1;
     }
-    printf("total player count: %d\n", league_players->count);
-    for (int i = 0; i < league_players->count; i++)
-    {
-        printf("Hrac ID: %d, Jmeno: %s\n", league_players->players[i].player_id, league_players->players[i].player_name);
-    }
 
     // read first line from match file and check if it is "match"
     fgets(buffer, buffer_size, file_match);
-    if (check_word(buffer, "match") != 0)
-    {
+    if (check_word(buffer, "match") != 0) {
         free(buffer);
         free(league_players->players);
         free(league_players);
@@ -137,8 +106,7 @@ int start_stats(const char* file_match_path, const char* file_player_names_path,
 
     // read second line from match file and check if read team players exist
     fgets(buffer, buffer_size, file_match);
-    if (player_exist(buffer, file_player_names) != 0)
-    {
+    if (player_exist(buffer, league_players) != 0) {
         free(buffer);
         free(league_players->players);
         free(league_players);
@@ -151,10 +119,9 @@ int start_stats(const char* file_match_path, const char* file_player_names_path,
     fgets(buffer, buffer_size, file_match);
     // TO DO: implement red team statistics check
 
-    // read fouth line from match file and check if blue team players exist
+    // read fourth line from match file and check if blue team players exist
     fgets(buffer, buffer_size, file_match);
-    if (player_exist(buffer, file_player_names) != 0)
-    {
+    if (player_exist(buffer, league_players) != 0) {
         free(buffer);
         free(league_players->players);
         free(league_players);
@@ -169,8 +136,7 @@ int start_stats(const char* file_match_path, const char* file_player_names_path,
 
     // read sixth line from match file and winner team
     fgets(buffer, buffer_size, file_match);
-    if (check_word(buffer, "winner_team") != 0)
-    {
+    if (check_word(buffer, "winner_team") != 0) {
         free(buffer);
         free(league_players->players);
         free(league_players);
